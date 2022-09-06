6211510a280cd57c940b6e898c94365626ab207e
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-09-06 06:33:48 -0700

    Add files via upload

--- src/components/useEffect/DataFetching.js
+++ src/components/useEffect/DataFetching.js
@@ -2,29 +2,40 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function DataFetching() {
-    const[posts, setPosts] = useState([]);
+    const[post, setPost] = useState({});
+    const[id, setId] = useState(1);
+    const[idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+    const handleClick = () => {
+        setIdFromButtonClick(id);
+    }
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
         .then(res => {
             console.log(res);
-            setPosts(res.data);
+            setPost(res.data);
         })
         .catch(err => {
             console.log(err);
         })
-    },[]);
+    },[idFromButtonClick]);
 
     return (
         <div>
             <h5>DataFetching</h5>
-            <ul>
+            <div>
+                <input type="text" value={id} onChange={e => setId(e.target.value)}/> 
+                <button type="button" onClick={handleClick} style={{marginLeft: '10px'}}>Fetch Post</button>
+            </div>
+            <div key={post.id}>{post.title}</div>
+            {/* <ul>
                 {
                     posts.map(post => (
                         <li key={post.id}>{post.title}</li>
                     ))
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
