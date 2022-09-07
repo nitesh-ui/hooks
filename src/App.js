e9a21d3b53ffcccf1505ef84cdc3e63c953eace2
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-09-07 04:59:03 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
+import ClassMouse from './components/ClassMouse';
+import ParentComponent from './components/useCallback/ParentComponent';
+import DataFetching from './components/useEffect/DataFetching';
+import HookCounterOne from './components/useEffect/HookCounterOne';
+import HookMouse from './components/useEffect/HookMouse';
+import Counter from './components/useMemo/Counter';
 import HookCounter from './components/useState/HookCounter';
 import HookCounterFour from './components/useState/HookCounterFour';
 import HookCounterThree from './components/useState/HookCounterThree';
@@ -13,6 +20,21 @@ function App() {
       <HookCounterTwo/>
       <HookCounterThree/>
       <HookCounterFour/>
+      <div className="App">
+        <ClassCounterOne/>
+        <HookCounterOne/>
+      </div>
+      <div className="App">
+        <ClassMouse/>
+        <HookMouse/>
+        <DataFetching/>
+      </div>
+      <div className="App">
+        <ParentComponent/>
+      </div>
+      <div className="App">
+        <Counter/>
+      </div>
     </div>
   );
 }
